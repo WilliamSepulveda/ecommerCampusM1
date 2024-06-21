a732a8f9cae14996f602da9a7c1851303add6ced
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-06-21 06:41:53 -0500

    chore: :construction: se corrige errores para que funcione en el servidor

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,15 +1,22 @@
-export const menuListCategoryIndex = (res)=>{
-    let {data} = res;
+export const menuListCategoryIndex = (res) => {
+    let { data } = res;
+    console.log(data);
     let plantilla = "";
-    data.forEach((value, index) =>{
-        plantilla += /*html*/`
-        <li title="${value.name}">
-            <a href="?id=${value.id}" >
-                <img src="storage/img/category.svg" >
-                <span> ${value.name} </span>
-            </a>
-        </li>
-        `;
-    });
+
+    if (Array.isArray(data)) {
+        data.forEach((value, index) => {
+            plantilla += /*html*/`
+            <li title="${value.name}">
+                <a href="?id=${value.id}" >
+                    <img src="storage/img/category.svg" >
+                    <span> ${value.name} </span>
+                </a>
+            </li>
+            `;
+        });
+    } else {
+        console.error('Data is not an array:', data);
+    }
+
     return plantilla;
 }
